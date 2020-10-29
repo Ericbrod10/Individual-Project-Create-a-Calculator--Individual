f3b88f97d023364fa6586aa2098425df7eadc825
Eric <56181235+Ericbrod10@users.noreply.github.com>
2020-10-28 23:42:18 -0400

    Feature: Setup Calculator with Add Method

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -5,7 +5,15 @@ from Calculator import Calculator
 class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         calculator = Calculator()
-        self.assertIsInstance(calculator,Calculator)
+        self.assertIsInstance(calculator, Calculator)
+
+    def test_results_property(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.result, 0)
+
+    def test_add_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(2, 2), 4)
 
 
 if __name__ == '__main__':
