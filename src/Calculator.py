9d0fc719edf48bd13a0f6edb8f3fe61360d682e0
Eric <56181235+Ericbrod10@users.noreply.github.com>
2020-10-29 00:01:45 -0400

    Feature: Setup Calculator With Subtraction

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,9 +1,24 @@
+def addition(a, b):
+    return float(a) + float(b)
+
+
+def subtraction(a, b):
+    a = float(a)
+    b = float(b)
+    c = a - b
+    return c
+
+
 class Calculator:
     result = 0
 
     def add(self, a, b):
-        c = a + b
-        return c
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
 
     def __init__(self):
         pass
